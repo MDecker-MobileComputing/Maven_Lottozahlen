cca0c7fb099da19fa1b06534b6ecd867e723abe8
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-11-27 08:41:00 +0100

    LottoZahlenGeneratorTest: neue Testmethode keineDoppeltenZahl()

--- src/test/java/de/eldecker/dhbw/lottozahlen/test/LottoZahlenGeneratorTest.java
+++ src/test/java/de/eldecker/dhbw/lottozahlen/test/LottoZahlenGeneratorTest.java
@@ -3,6 +3,9 @@ package de.eldecker.dhbw.lottozahlen.test;
 import static de.eldecker.dhbw.lottozahlen.engine.LottoZahlenGenerator.TIPP_ANZAHL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +14,10 @@ import de.eldecker.dhbw.lottozahlen.engine.LottoZahlenGenerator;
 
 public class LottoZahlenGeneratorTest {
         
+    /**
+     * Reihenfolge der Testausführung ist nicht deterministisch, deshalb
+     * vor jeder Testmethode den Zufallsgenerator neu initialisieren.
+     */
     @BeforeEach
     void prepareBeforeEachUnitTest() {
         
@@ -20,9 +27,29 @@ public class LottoZahlenGeneratorTest {
     @Test
     void richtigeAnzahl() {
     
-        int[] ergebnisArray = LottoZahlenGenerator.erzeugeTipp();
-        assertEquals(TIPP_ANZAHL, ergebnisArray.length, 
-                     "Ergebnis-Array hat nicht erwartete Länge");
+        for (int i = 1; i <= 10; i++) {
+
+            int[] ergebnisArray = LottoZahlenGenerator.erzeugeTipp();
+            assertEquals(TIPP_ANZAHL, ergebnisArray.length, 
+                         "Ergebnis-Array hat nicht erwartete Länge in Durchlauf " + i);
+        }
+    }
+    
+    @Test
+    void keineDoppeltenZahl() {
+
+        for (int i = 1; i <= 10; i++) {
+
+            int[] ergebnisArray = LottoZahlenGenerator.erzeugeTipp();
+            
+            Set<Integer> intSet = new HashSet<>(TIPP_ANZAHL);
+            for (int wert : ergebnisArray) {
+                intSet.add(wert);
+            }
+            
+            assertEquals(TIPP_ANZAHL, intSet.size(),
+                         "Ergebnis-Array hat mehrfache Einträge in Durchlauf " + i);
+        }
     }
     
     
